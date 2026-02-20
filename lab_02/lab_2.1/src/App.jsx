b558513f8b3d4aeadf79e2bc99a72c09d1c555f2
mindcore <cardinall1304@gmail.com>
2026-02-21 00:56:03 +0500

    feat: обновить компонент App для отображения списка продуктов с использованием Section и ProductList

--- lab_02/lab_2.1/src/App.jsx
+++ lab_02/lab_2.1/src/App.jsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import CombinedComponent from "./components/CombinedComponent";
 import FragmentLayout from "./components/FragmentLayout";
+import './App.css';
 
 
 function App() {
